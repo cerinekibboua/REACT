c4c3e7c8cdc26132767255a6b19b48598b1a7b91
cerine kibboua <cerinekibboua65@gmail.com>
2021-04-22 11:20:45 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import Form from './components/formulaire'
+import Formulaire from './components/formulaire';
+import Navbars from './components/navbars'
+import Foo from './components/footer'
+    function App() {
+      return (
+        <div>
+        <Navbars></Navbars>
+        <Formulaire></Formulaire>
+       <Foo></Foo>
+        </div>
+      );
+    }
+    export default App;
+
+
+
+
+
+
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import Something from './components/something'
+import Exo1 from './components/exo1'
+import Image from './components/logo'
+import Sub from './components/subscribe'
+ReactDOM.render(
+ 
+   <Sub></Sub>
+  ,
+  document.getElementById('root')
+);
+
+
